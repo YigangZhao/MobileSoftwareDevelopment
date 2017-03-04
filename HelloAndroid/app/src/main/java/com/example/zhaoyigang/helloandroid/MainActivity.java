288199ba65b55e3a479b549a33c364d805e62137
zhaoyigang <992261755@qq.com>
2017-03-04 15:48:59 +0800

    firstcommit

---
+++ HelloAndroid/app/src/main/java/com/example/zhaoyigang/helloandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhaoyigang.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
